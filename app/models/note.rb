c170d4c847eba814ebdf6764a1fe5c724a8a6fe0
Chris Berkhout <chrisberkhout@gmail.com>
2011-08-29 15:56:52 +1000

    Added some comments.

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,24 +4,29 @@ class Note
   include Tire::Model::Callbacks
   
   index_name ES_INDEX_NAME
-  
+
+  # Refresh ES so any changes are immediately visible
   refresh = lambda { Tire::Index.new(ES_INDEX_NAME).refresh }
   after_save &refresh
   after_destroy &refresh
 
   property :body
-  
   property :updated_at
+  
   before_save { |n| n.updated_at = Time.now }
   
   def self.touch_es
+    # Ensure a mapping in a fresh index, so that Note.all can sort on updated_at
     n = Note.new
     n.save
     n.destroy
   end
+  
   def self.all
+    # Override so that Note.all comes back sorted on updated_at, rather than _id
     search { sort { by :updated_at, 'desc' } }
   end
+  
   def self.q(q)
     search { sort { by :updated_at, 'desc' }; query { string q } }
   end
